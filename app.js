b487bf8ae5fd72f68485b11155b07ac99cb8ed3f
Tony Melek <tonymelek.au@gmail.com>
2024-01-11 17:17:48 +1100

    fix styling

--- app.js
+++ app.js
@@ -1,51 +1,67 @@
-const commonActivities1=[
-    {from:'08:30:00',to: '10:30:00', name:'Divine Liturgy'},
-    {from:'10:30:00',to: '10:45:00', name:'Registration'},
-    {from:'10:45:00',to: '11:15:00', name:'Breakfast'},
-    {from:'11:15:00',to: '12:00:00', name:'Greeting and spritual Topic'}
+const commonActivities1 = [
+    { from: '08:30:00', to: '10:30:00', name: 'Divine Liturgy' },
+    { from: '10:30:00', to: '10:45:00', name: 'Registration' },
+    { from: '10:45:00', to: '11:15:00', name: 'Breakfast' },
+    { from: '11:15:00', to: '12:00:00', name: 'Greeting and spritual Topic' }
 ]
-const lunch={
-    from:'14:00:00',to: '14:30:00', name:'Lunch'  
+const lunch = {
+    from: '14:00:00', to: '14:30:00', name: 'Lunch'
 }
-const closing={
-    from:'16:00:00',to: '16:30:00', name:'Closing and Group photo'
+const closing = {
+    from: '16:00:00', to: '16:30:00', name: 'Closing and Group photo'
 }
-const grpAct={
-    smallKids:[
+const grpAct = {
+    smallKids: [
         ...commonActivities1,
-        {from:'12:00:00',to: '14:00:00', name:'Entertainment',list:['Jumping Castle','Face Painting','Craft Art']},
+        { from: '12:00:00', to: '14:00:00', name: 'Entertainment', list: ['Jumping Castle', 'Face Painting', 'Craft Art'] },
         lunch,
+        { from: '14:30:00', to: '16:00:00', name: 'Entertainment', list: ['Jumping Castle', 'Face Painting', 'Craft Art'] },
         closing
-     ],
-     midKids:[
+    ],
+    midKids: [
         ...commonActivities1,
-        {from:'12:00:00',to: '14:00:00', name:'Entertainment',list:['Jumping Castle','Soccer','Face Painting']},
+        { from: '12:00:00', to: '14:00:00', name: 'Entertainment', list: ['Jumping Castle', 'Soccer', 'Face Painting'] },
         lunch,
+        { from: '14:30:00', to: '16:00:00', name: 'Entertainment', list: ['Jumping Castle', 'Soccer', 'Face Painting'] },
         closing
-     ],highschool:[
+    ], highschool: [
         ...commonActivities1,
-        {from:'12:00:00',to: '14:00:00', name:'Entertainment',list:['Soccer','Tennis Table','Basket Ball']},
+        { from: '12:00:00', to: '14:00:00', name: 'Entertainment', list: ['Soccer', 'Tennis Table', 'Basket Ball'] },
         lunch,
+        { from: '14:30:00', to: '16:00:00', name: 'Entertainment', list: ['Soccer', 'Tennis Table', 'Basket Ball'] },
         closing
-     ]
+    ]
 }
+
 const app = {
     data() {
         return {
-         grpAct,
-         group:''
+            grpAct,
+            group: ''
         }
     },
-    mounted(){
+    mounted() {
         const urlParams = new URLSearchParams(window.location.search);
-        this.group= urlParams.get('group');
-        
-    },
-    computed: {
+        this.group = urlParams.get('group');
 
     },
+    computed: {
+        activePeriodIndex() {
+            const timeNow = new Intl.DateTimeFormat('en', { hourCycle: 'h24', hour: "numeric", minute: "numeric", second: "numeric", timeZone: "Australia/Sydney" }).format(new Date()).toString();
+            return this.grpAct.smallKids.findIndex(act => {
+                // console.log(act.from, timeNow,act.from< timeNow,act.to,act.to> timeNow);
+                return act.from < timeNow && act.to > timeNow
+            })
+
+        }
+    },
     methods: {
- 
+        getClass(index){
+            console.log(index,this.activePeriodIndex);
+            if(index<this.activePeriodIndex) return 'bg-red';
+            if(index===this.activePeriodIndex) return 'bg-green';
+            return 'bg-yellow'
+        }
     }
 
 }
