f591408f56d0b6e75f9c401de8582728ebcc8c3d
Tony Melek <tonymelek.au@gmail.com>
2024-01-11 21:42:48 +1100

    add venues

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const commonActivities1 = [
     { from: '08:30:00', to: '10:30:00', name: 'Divine Liturgy' },
     { from: '10:30:00', to: '10:45:00', name: 'Registration' },
     { from: '10:45:00', to: '11:15:00', name: 'Breakfast' },
-    { from: '11:15:00', to: '12:00:00', name: 'Greeting and spritual Topic' }
+    { from: '11:15:00', to: '12:00:00', name: 'Greeting and spritual Topic', venue: 'St. Athanasious Church' }
 ]
 const lunch = {
     from: '14:00:00', to: '14:30:00', name: 'Lunch'
@@ -15,20 +15,23 @@ const grpAct = {
         ...commonActivities1,
         { from: '12:00:00', to: '14:00:00', name: 'Entertainment', list: ['Jumping Castle', 'Face Painting', 'Craft Art'] },
         lunch,
-        { from: '14:30:00', to: '16:00:00', name: 'Entertainment', list: ['Jumping Castle', 'Face Painting', 'Craft Art'] },
+        { from: '14:30:00', to: '14:50:00', name: 'Spititual activity', venue: 'Pope Shenouda III-Auditorium' },
+        { from: '14:50:00', to: '16:00:00', name: 'Entertainment', list: ['Jumping Castle', 'Face Painting', 'Craft Art'] },
         closing
     ],
     midKids: [
         ...commonActivities1,
         { from: '12:00:00', to: '14:00:00', name: 'Entertainment', list: ['Jumping Castle', 'Soccer', 'Face Painting'] },
         lunch,
-        { from: '14:30:00', to: '16:00:00', name: 'Entertainment', list: ['Jumping Castle', 'Soccer', 'Face Painting'] },
+        { from: '14:30:00', to: '14:50:00', name: 'Spititual activity', venue: 'St. Athanasious Coptic Theological College' },
+        { from: '14:50:00', to: '16:00:00', name: 'Entertainment', list: ['Jumping Castle', 'Soccer', 'Face Painting'] },
         closing
     ], highschool: [
         ...commonActivities1,
         { from: '12:00:00', to: '14:00:00', name: 'Entertainment', list: ['Soccer', 'Tennis Table', 'Basket Ball'] },
         lunch,
-        { from: '14:30:00', to: '16:00:00', name: 'Entertainment', list: ['Soccer', 'Tennis Table', 'Basket Ball'] },
+        { from: '14:30:00', to: '14:50:00', name: 'The Eleventh hour Prayer', venue: 'St. Athanasious Church' },
+        { from: '14:50:00', to: '16:00:00', name: 'Entertainment', list: ['Soccer', 'Tennis Table', 'Basket Ball'] },
         closing
     ]
 }
@@ -56,10 +59,9 @@ const app = {
         }
     },
     methods: {
-        getClass(index){
-            console.log(index,this.activePeriodIndex);
-            if(index<this.activePeriodIndex) return 'bg-red';
-            if(index===this.activePeriodIndex) return 'bg-green';
+        getClass(index) {
+            if (index < this.activePeriodIndex) return 'bg-red';
+            if (index === this.activePeriodIndex) return 'bg-green';
             return 'bg-yellow'
         }
     }
